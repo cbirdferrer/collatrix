aa9d56f93278eec753a1a7fa4307ed621e7c3ab3
Clara <clara.birdferrer@gmail.com>
2020-05-19 17:50:28 -0400

    updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "collatrix",
-    version = "1.0.4",
+    version = "1.0.5",
     description="A GUI for collating MorphoMetriX outputs",
     author = "Clara Bird",
     author_email = "clara.birdferrer@gmail.com",
