a4e781c7fcf21671e183f4d43b5c50fa7970495e
cbirdferrer <clara.birdferrer@gmail.com>
2021-06-15 10:35:56 -0700

    updating test

--- collatrix/tests/test_collatrix.py
+++ collatrix/tests/test_collatrix.py
@@ -30,6 +30,6 @@ anFold = 'no'
 constants = ['Image ID', 'Image Path', 'Focal Length', 'Altitude', 'Pixel Dimension']
 
 def test_col():
-    dfx = collate_v6(csvs, 'Object',  'Length (m)',constants,safety,df_L,measurements, nonPercMeas, anFold)
+    dfx,dfx_pc = collate_v6(csvs, 'Object',  'Length (m)',constants,safety,df_L,measurements, nonPercMeas, anFold)
     return(print(dfx))
     assert dfx.empty == False
